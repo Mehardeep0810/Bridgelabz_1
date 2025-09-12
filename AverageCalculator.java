635f3744a722ce4fa5b35eec072bf2a21c660e66
Mehardeep Kaur <mehardeepk.38@gmail.com>
2025-09-12 15:11:17 +0530

    AverageCalculator.java

--- AverageCalculator.java
+++ AverageCalculator.java
@@ -6,7 +6,6 @@ public class AverageCalculator {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        // Input three numbers
         System.out.print("Enter first number: ");
         double num1 = sc.nextDouble();
 
@@ -16,12 +15,11 @@ public class AverageCalculator {
         System.out.print("Enter third number: ");
         double num3 = sc.nextDouble();
 
-        // Calculate average
         double average = (num1 + num2 + num3) / 3;
 
-        // Display result
         System.out.println("Average of the three numbers: " + average);
 
         sc.close();
     }
+
 }
